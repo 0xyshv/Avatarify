c8abeaefd8effe3d78e51f59f5404b44a5e3414f
0xyshv <writetoyshv@gmail.com>
2023-12-17 15:14:49 +0530

    feat: add home route

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,18 @@
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/home");
+  };
+
   return (
     <div className="flex justify-center items-center h-screen">
-      <button className="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded">
+      <button
+        className="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded"
+        onClick={handleClick}
+      >
         Enter App
       </button>
     </div>
