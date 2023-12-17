3eecd50eae36109ff3f042eb78e62015f9e2a00d
0xyshv <writetoyshv@gmail.com>
2023-12-17 19:42:39 +0530

    sync

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,18 +1,17 @@
 function Navbar() {
   return (
     <nav className="flex justify-between items-center w-full  sticky top-0 z-40  ">
-      <div className="ml-2">
+      <div className="ml-2 flex items-center gap-2">
         <img
           src="logo.png"
           alt="Default Avatar"
           style={{ height: "40px", width: "40px" }}
         />
-      </div>
-      <div>
         <h1 className="font-bold text-3xl p-2 sm:p-4  text-violet-100">
           Pixels
         </h1>
       </div>
+      <div></div>
     </nav>
   );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -11,8 +11,10 @@ export default function Home() {
     <div className="flex flex-col gap-8 justify-center items-center mt-48">
       <div className="font-semibold text-2xl text-violet-100">
         Pixels lets you generate cool avatars from your profile pictures using
-        AI. Users can upload their images and enter a prompt, and then the AI
-        would generate avatar for it.
+        AI.
+        <br />
+        Users can upload their images and enter a prompt, and then the AI would
+        <br /> generate avatar for it.
       </div>
       <button
         className="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded"
