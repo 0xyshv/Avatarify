d042eefbb7e1b716f1f64b457e1739837a301ef1
0xyshv <writetoyshv@gmail.com>
2023-12-17 16:19:44 +0530

    feat: add prompt form

---
+++ components/promptForm.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export default function PromptForm({ onSubmit, scribbleExists }) {
+  const [prompt, setPrompt] = useState();
+
+  const disabled = !(scribbleExists && prompt?.length > 0);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(e);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="animate-in fade-in duration-700">
+      <div className="flex mt-4">
+        <input
+          id="prompt-input"
+          type="text"
+          name="prompt"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+          placeholder="Describe the avatar you want to create..."
+          className="block w-full flex-grow rounded-l-md text-cyan-950"
+        />
+
+        <button
+          className={`bg-cyan-950 text-teal-100 rounded-r-md text-small inline-block px-5 py-3 flex-none ${
+            disabled ? "opacity-20 cursor-not-allowed	" : ""
+          }`}
+          type="submit"
+          disabled={disabled}
+        >
+          Generate Avatar
+        </button>
+      </div>
+    </form>
+  );
+}
--- pages/home.jsx
+++ pages/home.jsx
@@ -1,16 +1,85 @@
 import React, { useState } from "react";
 import ImageUploader from "components/uploadImage";
+import PromptForm from "components/promptForm";
 
 const Main = () => {
   const [image, setImage] = useState(null);
   const [imageExists, setImageExists] = useState(false);
+  const [isProcessing, setIsProcessing] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // track submissions so we can show a spinner while waiting for the next prediction to be created
+    setSubmissionCount(submissionCount + 1);
+
+    const prompt = e.target.prompt.value
+      .split(/\s+/)
+      .map((word) => (naughtyWords.en.includes(word) ? "something" : word))
+      .join(" ");
+
+    setError(null);
+    setIsProcessing(true);
+
+    const fileUrl = await uploadFile(image);
+
+    const body = {
+      prompt: "In the style of TOK, " + prompt,
+      image: fileUrl,
+    };
+
+    const response = await fetch(`${window.location.href}/api/predictions`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+    let prediction = await response.json();
+
+    setPredictions((predictions) => ({
+      ...predictions,
+      [prediction.id]: prediction,
+    }));
+
+    if (response.status !== 201) {
+      setError(prediction.detail);
+      return;
+    }
+
+    while (
+      prediction.status !== "succeeded" &&
+      prediction.status !== "failed"
+    ) {
+      await sleep(500);
+      const response = await fetch("/api/predictions/" + prediction.id);
+      prediction = await response.json();
+      setPredictions((predictions) => ({
+        ...predictions,
+        [prediction.id]: prediction,
+      }));
+      if (response.status !== 200) {
+        setError(prediction.detail);
+        return;
+      }
+    }
+
+    setIsProcessing(false);
+  };
 
   return (
-    <ImageUploader
-      setImage={setImage}
-      imageExists={imageExists}
-      setImageExists={setImageExists}
-    />
+    <>
+      <ImageUploader
+        setImage={setImage}
+        imageExists={imageExists}
+        setImageExists={setImageExists}
+      />
+      <PromptForm
+        onSubmit={handleSubmit}
+        isProcessing={isProcessing}
+        scribbleExists={imageExists}
+      />
+    </>
   );
 };
 
