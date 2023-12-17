60d059f4bf5ee74318d6ecb114f04ea7a66b10df
0xyshv <writetoyshv@gmail.com>
2023-12-17 19:44:08 +0530

    sync

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,11 +10,13 @@ export default function Home() {
   return (
     <div className="flex flex-col gap-8 justify-center items-center mt-48">
       <div className="font-semibold text-2xl text-violet-100">
-        Pixels lets you generate cool avatars from your profile pictures using
-        AI.
+        <span className="ml-28">
+          Pixels lets you generate cool avatars from your profile pictures using
+          AI.
+        </span>
         <br />
         Users can upload their images and enter a prompt, and then the AI would
-        <br /> generate avatar for it.
+        generate avatar for it.
       </div>
       <button
         className="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded"
